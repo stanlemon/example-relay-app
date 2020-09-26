6c74e1b613587dc8a3ca26573d921dbdf9568dbd
Stan Lemon <stanlemon@users.noreply.github.com>
2020-09-26 08:58:54 -0400

    Switch to webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: ["./src/index.js", "react-hot-loader/patch"],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  devtool:
+    process.env.NODE_ENV === "production" ? "source-map" : "eval-source-map",
+  devServer: {
+    hot: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.([j|t]s)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        loader: "url-loader",
+      },
+    ],
+  },
+  resolve: {
+    // Enable webpack to find files without these extensions
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ],
+};
